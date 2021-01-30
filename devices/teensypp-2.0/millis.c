99fb36d8fc13f38ca39fc74f09325efd346f1c65
Álvaro Fernández Rojas <noltari@gmail.com>
2021-01-30 18:09:14 +0100

    teensy++ 2.0: use BIT macro
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- devices/teensypp-2.0/millis.c
+++ devices/teensypp-2.0/millis.c
@@ -5,6 +5,8 @@
 #include <util/atomic.h>
 #include <stdint.h>
 
+#include "common.h"
+
 volatile uint32_t timer_ms; 
 
 ISR(TIMER1_COMPA_vect)
@@ -16,12 +18,12 @@ void millis_init(void)
 {
 	uint32_t ctc = ((F_CPU / 1000) / 8);
 
-	TCCR1B |= (1 << WGM12) | (1 << CS11);
+	TCCR1B |= BIT(WGM12) | BIT(CS11);
 
 	OCR1AH = (ctc >> 8);
 	OCR1AL = ctc;
  
-	TIMSK1 |= (1 << OCIE1A);
+	TIMSK1 |= BIT(OCIE1A);
 }
 
 uint32_t millis(void)
